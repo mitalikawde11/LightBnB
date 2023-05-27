4724d3d4a6b523e43f6e26468044e0fbfb55aa6c
Mitali <mitali.kawde11@gmail.com>
2023-05-27 00:56:30 -0700

    Refactoring the getAllReservations function to use the lightbnb database to return reservations associated with a specific user

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -27,7 +27,7 @@ const getUserWithEmail = function (email) {
       WHERE email = $1;
     `, [email])
     .then((result) => {
-      console.log("Get user with email: ", result.rows[0]);
+      // console.log("Get user with email: ", result.rows[0]);
       return result.rows[0];
     })
     .catch((err) => {
@@ -84,7 +84,7 @@ const addUser = function (user) {
       RETURNING *;
     `, [user.name, user.email, user.password])
     .then((result) => {
-      console.log("Result: ", result.rows[0]);
+      // console.log("Result: ", result.rows[0]);
       return result.rows[0];
     })
     .catch((err) => {
@@ -107,7 +107,27 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+    SELECT reservations.*, properties.*, ROUND(AVG(rating), 1) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+    `, [guest_id, limit])
+    .then((result) => {
+      // console.log("All reservations: ", result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  
+  
+  // return getAllProperties(null, 2);
 };
 
 /// Properties
