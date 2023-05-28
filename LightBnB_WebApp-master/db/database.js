7d4e7e6c7734faedb4a7965319d9d6741362e66f
Mitali <mitali.kawde11@gmail.com>
2023-05-27 17:14:50 -0700

    Updating addProperty function to save a new property to the properties table

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -211,7 +211,7 @@ const getAllProperties = function (options, limit = 10) {
     `;
   }
 
-  console.log(queryString, queryParams);
+  // console.log(queryString, queryParams);
     
   return pool.query(queryString, queryParams)
     .then((result) => {
@@ -229,10 +229,20 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+    .query(`
+      INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;
+    `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night * 100, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then((result) => {
+      console.log("Adding property: ", result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+      // console.error('query error', err.stack);
+    })
 };
 
 module.exports = {
